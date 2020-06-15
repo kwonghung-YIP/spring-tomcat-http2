8b7f1054391b72685dfac3304b42ec12b7817856
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2020-06-15 23:59:07 +0800

    revised echo message

--- src/main/java/org/hung/SimpleController.java
+++ src/main/java/org/hung/SimpleController.java
@@ -1,15 +1,28 @@
 package org.hung;
 
-import javax.servlet.ServletRequest;
+import java.util.Collections;
+import java.util.Enumeration;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.PushBuilder;
+
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class SimpleController {
 
 	@GetMapping("/echo")
-	public String echo(ServletRequest request) {
-		return "echo\n"+request.getRemoteAddr()+"\n";
+	public String echo( HttpServletRequest request, PushBuilder pushBuilder ) {
+		for (Enumeration<String> eh=request.getHeaderNames();eh.hasMoreElements();) {
+			String name = eh.nextElement();
+			log.info("{}:[{}]",name,StringUtils.collectionToCommaDelimitedString(Collections.list(request.getHeaders(name))));
+		}
+		log.info("PushBuilder reference:" + pushBuilder);
+		return "You are from " + request.getRemoteAddr() + " using " + request.getProtocol();
 	}
 }
